d6073f75f805c0ca8f4190b2c58c489e7739ef85
Daniel Lee <daniel.lee@kabaminc.com>
2014-06-27 16:13:04 -0700

    Added html label

--- web.js
+++ web.js
@@ -1,14 +1,23 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var http = require("http");
+var port = process.env.PORT || 8000;
 
-app.use(logfmt.requestLogger());
+var counter = 0;
 
-app.get('/', function(req, res) {
-   res.send('Hello World!');
-});
+http.createServer(function (request, response) {
+   counter++;
+   
+   var path = request.url;
+   console.log("requested=" + path + " counter= " + counter);
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-   console.log("Listening on " + port);
-});
+   response.writeHead(200, {'Content-Type': 'text/html'});
+
+   if (path == "/") {
+      response.end("Hello World. You are requestor #" + counter + 
+         ".<br><a href='/page2'>Page 2</a>\n");
+   }
+   else if (path == "/page2") {
+      response.end("This is page 2. <a href='/'>Back.</a>\n");
+   }
+}).listen(port);
+
+console.log("Server running at http://127.0.0.1:" + port);
